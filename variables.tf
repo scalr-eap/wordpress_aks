5bbacefcdce76fddfb124efdd62d654833642505
Ryan <ryan.fee625@gmail.com>
2020-01-15 10:24:05 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {}
+variable "cluster_name" {}
+variable "resource_group" {}
+variable "mysql_password" {}
+variable "service_name" {}
+variable "mysql_name" {}
+variable "mysql_sku_name" {}
+variable "mysql_capacity" {}
+variable "mysql_username" {}
