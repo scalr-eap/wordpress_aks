b896748d790c145beab46efc9ff870e1aab40017
Ryan <ryan.fee625@gmail.com>
2020-01-15 10:20:59 -0500

    Create sql.tf

---
+++ sql.tf
@@ -0,0 +1,23 @@
+resource "azurerm_mysql_server" "default" {
+  name                = var.mysql_name
+  location            = var.region
+  resource_group_name = var.resource_group
+
+  sku {
+    name     = var.mysql_sku_name
+    capacity = var.capacity
+    tier     = "Basic"
+    family   = "Gen5"
+  }
+
+  storage_profile {
+    storage_mb            = 5120
+    backup_retention_days = 7
+    geo_redundant_backup  = "Disabled"
+  }
+
+  administrator_login          = var.mysql_username
+  administrator_login_password = var.mysql_pass
+  version                      = "5.7"
+  ssl_enforcement              = "Enabled"
+}
